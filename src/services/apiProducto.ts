54b127d942aeb97af0258a6cf688b914d203bb59
Santos <nunezsantos@javeriana.edu.co>
2021-06-04 14:59:16 -0500

    pre merge Main

--- src/services/apiProducto.ts
+++ src/services/apiProducto.ts
@@ -18,4 +18,22 @@ export const apiProducto = {
       throw new Error('Error al cargar los productos: '.concat(e));
     }
   },
+  async getProductoFind(idProducto: string): Promise<Producto> {
+    try {
+      const response = await axios.get(process.env.VUE_APP_BASE_URL.concat('/producto/').concat(idProducto),
+      {
+        headers: {
+          Authorization: 'Bearer ' + Cookies.get('token')
+        }
+      });
+      console.log(response.data);
+      return new Promise((resolve) => {
+        resolve(response.data);
+      });
+    }
+    catch (e) {
+      throw new Error('Error al cargar Producto: '.concat(e));
+    }
+  }
+
 };
